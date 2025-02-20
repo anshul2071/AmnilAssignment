46c36eeac368b381c4a6ec3fb7a0b46c04d65259
Anshul Rawal <104826906+anshul2071@users.noreply.github.com>
2025-02-20 13:40:30 +0545

    Update getandset.js

--- Objects/getandset.js
+++ Objects/getandset.js
@@ -1,25 +1,21 @@
-//object with owner name and balance with get balance with getter and setter 
-
 const account = {
-    owner: "John",
-    balance: 1000,
-    
-    get balance() {
-      return `Balance: $${this.balance}`;
-    },
+  owner: "Tripti",
+  _balance: 1000,
   
-    set balance(amount) {
-      if (amount < 0) {
-        console.log("Invalid balance!");
-      } else {
-        this.balance = amount;
-      }
-    }
-  };
-  
-  console.log(account.balance); 
-  account.balance = 500;
-  console.log(account.balance); 
-  account.balance = -200; 
-  
+  get balance() {
+    return `Balance: $${this._balance}`;
+  },
 
+  set balance(amount) {
+    if (amount < 0) {
+      console.log("Invalid balance!");
+    } else {
+      this._balance = amount;
+    }
+  }
+};
+
+console.log(account.balance); 
+account.balance = 500;
+console.log(account.balance); 
+account.balance = -200; 
